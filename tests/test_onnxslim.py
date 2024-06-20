ce61cb15f01588439226d162ba71cfca85040ce5
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-06-20 23:08:44 +0200

    Refactor code for speed and clarity

--- tests/test_onnxslim.py
+++ tests/test_onnxslim.py
@@ -31,7 +31,6 @@ class TestOnnxModel:
     def test_onnxslim_python_api(self, request, name):
         """Tests the ONNX model slimming Python API using the 'onnxslim' command for a given model name."""
         import onnx
-
         from onnxslim import slim
 
         filename = download_onnx_from_url(f"http://120.224.26.32:15030/aifarm/onnx/{name}.onnx")
